2e0d71c2d9ddab857f2951a0fa756d79610f85e3
Strucker.eth <aberamacarleton@gmail.com>
2023-04-06 22:41:29 +0300

    BSC main and testnet add

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,12 @@ import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultWallets, RainbowKitProvider, 
 } from '@rainbow-me/rainbowkit';
 import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { mainnet, polygon, optimism, arbitrum, polygonMumbai } from 'wagmi/chains';
+import { mainnet, polygon, optimism, arbitrum, polygonMumbai, bsc, bscTestnet} from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
 const { chains, provider } = configureChains(
-  [mainnet, polygon, optimism, arbitrum],
+  [mainnet, polygon, optimism, arbitrum, polygonMumbai, bsc, bscTestnet],
   [
     alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
     publicProvider()
