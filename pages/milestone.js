f9948a9c0004b30c8d736b9f37e11da3248468f4
Strucker.eth <132390630+Fli-ick@users.noreply.github.com>
2023-05-10 12:09:00 +0300

    Navbar positioning

--- pages/milestone.js
+++ pages/milestone.js
@@ -14,10 +14,15 @@ export default function Home() {
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet"></link>
     </head>
 
-    <div id="navbar" class="rounded-xl p-3  bg-white  mx-auto px-4 py-0">
+   
+      <div className="bg-hero-top h-[900px] bg-no-repeat bg-top-4 bg-50% ">
+      <div id="navbar" class="rounded-xl p-3  bg-white  mx-8 px-4 py-0">
 <Navbar />
 </div>
-      <div className="bg-hero-top h-[900px] bg-no-repeat bg-top-4 bg-50% ">
+
+
+
+
         </div>
       <footer>
         {/* Add your footer content here */}
