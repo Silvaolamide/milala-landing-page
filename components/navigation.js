876c0edd04f3a363094615ce91ec0847199fb825
Strucker.eth <132390630+Fli-ick@users.noreply.github.com>
2023-05-10 12:07:22 +0300

    Milestone Page

--- components/navigation.js
+++ components/navigation.js
@@ -4,21 +4,21 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 const Navbar = () => {
   return (
-    <div  class="text-slate-700 container relative mx-auto   flex flex-col overflow-hidden px-2 py-2 lg:flex-row lg:items-center">    
-    <div className="flex justify-center items-end"> 
+    <div  class="text-slate-700 container relative mx-auto rounded-xl  flex flex-col overflow-hidden px-2 py-2 lg:flex-row lg:items-center">    
+    <div id =" logo" className="flex justify-center items-end"> 
           <img className="w-7 mx-1 my-0" src="./img/auspicious-blockchain-logo.svg" alt="Auspicious Blockchain Logo" />
         </div>
         <div className="font-Inter items-start px-3 text-xl font-extra-bold text-[#009A9A] text-right ">
            Milala
         
         </div>
-                      
+                     
         <nav aria-label="Header Navigation" font-Inter font-extra-bold class="peer-checked:pt-4  peer-checked:max-h-60 flex max-h-0 w-10/12 flex-col items-center overflow-hidden transition-all lg:ml-24 lg:max-h-full lg:flex-row">
   <ul class="flex w-10/12 bg-transparent flex-col items-center space-y-2 lg:flex-row lg:justify-center lg:space-y-0">
     <li class="lg:mr-12"><a class="rounded -ml-20 text-gray transition focus:outline-none focus:ring-1 focus:ring-blue-700 focus:ring-offset-2" href="#">Projects</a></li>
     <li class="lg:mr-12"><a class="rounded text-gray transition focus:outline-none focus:ring-1 focus:ring-blue-700 focus:ring-offset-2" href="#">Investments</a></li>
     <li class="lg:mr-12"><a class="rounded text-gray-700 transition focus:outline-none focus:ring-1 focus:ring-blue-700 focus:ring-offset-2" href="#">Register</a></li>
-    <li class="lg:mr-12"><a class="rounded text-gray-700 transition focus:outline-none focus:ring-1 focus:ring-blue-700 focus:ring-offset-2" href="#">FAQ</a></li>
+    <li class="lg:mr-12"><a class="rounded text-gray-700 transition focus:outline-none focus:ring-1 focus:ring-blue-700 focus:ring-offset-2" href="/milestone">Milestones</a></li>
   </ul>
   <hr class=" w-full lg:hidden" />
   <div class=" flex items-center space-x-6 space-y-2 lg:my-0 lg:ml-auto lg:space-x-8 lg:space-y-0">
--- pages/milestone.js
+++ pages/milestone.js
@@ -2,106 +2,26 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-
-
-
+import Navbar from '../components/navigation';
 
 export default function Home() {
   return (
     <>
-    <head>
-          <title>Milala</title>
+       <head>
+          <title>Milestones</title>
           <link rel="icon" href="/favicon.ico" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;500;700;800;900&display=swap" rel="stylesheet"/>
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap" rel="stylesheet"></link>
     </head>
-    <div className="bg-hero-top h-[900px] bg-no-repeat bg-top-4 bg-50% ">
-    <header className="pt-9">
-   
-      <div  className="flex ml-10 h-auto border-red-400 items-start flex-start justify-start mx-auto ">    
-          <div className="font-Inter  text-white text-right ">
 
-            <span className="text-smm text-#[009A9A] font-extra-bold block leading-3">Auspicious Blockchain</span>
-            <span className="text-xss font-extra-bold  text-[#009A9A] block">Auspicious Blockchain Pte Ltd</span>
-
-          </div>
-         
-          
-          <div className="flex justify-center items-end"> 
-            <img className="w-7 mx-2 my-0" src="./img/auspicious-blockchain-logo.svg" alt="Auspicious Blockchain Logo" />
-          </div>
-      </div>
-
-      <div className="justify-end flex flex-end items-end -mt-8 mr-3 sm:mr-10 sm:justify-end  md:justify-end sm:flex-end sm:items-end">
-      <ConnectButton />
-         </div>
-</header>
-
-     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
-      
-        
-   
-        <main >
-       
-              
-            
-        </main>
-        
-    <footer>
- 
-
-          </footer>
-      
-    </div>
-
+    <div id="navbar" class="rounded-xl p-3  bg-white  mx-auto px-4 py-0">
+<Navbar />
+</div>
+      <div className="bg-hero-top h-[900px] bg-no-repeat bg-top-4 bg-50% ">
+        </div>
+      <footer>
+        {/* Add your footer content here */}
+      </footer>
     </>
-  )
+  );
 }
