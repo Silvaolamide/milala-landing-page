abf6f20a4509e3df63e2fdc9aaf40d775d052d9a
strucker-eth <aberamacarleton@gmail.com>
2023-05-19 17:40:50 +0300

    sidebar items addition

--- components/sidebar.js
+++ components/sidebar.js
@@ -10,38 +10,46 @@ const Dboard = () => {
           <Sidebar.Items>
             <Sidebar.ItemGroup>
               <Sidebar.Item href="#" icon={HiChartPie}>
-                <h3 class="text-xl font-semibold text-gray-800">Dashboard</h3>
+                <h3 class="text-xl font-semibold text-gray-800">Projects Management</h3>
               </Sidebar.Item>
               <Sidebar.Item href="#" icon={HiInbox}>
-                <h3 class="text-xl font-semibold text-gray-800">Inbox</h3>
+                <h3 class="text-xl font-semibold text-gray-800">My Projects</h3>
               </Sidebar.Item>
-              <Sidebar.Collapse icon={HiShoppingBag} label="E-commerce">
+              <Sidebar.Collapse icon={HiShoppingBag} label="Projects' Management">
                 <Sidebar.Item href="#">
-                  <h3 class="text-xl font-semibold text-gray-800">Products</h3>
+                  <h3 class="text-xl font-semibold text-gray-800">Add Project</h3>
+                </Sidebar.Item>
+                <Sidebar.Item href="#">
+                  <h3 class="text-xl font-semibold text-gray-800">Update Project Milestones</h3>
                 </Sidebar.Item>
               </Sidebar.Collapse>
-              <Sidebar.Collapse icon={HiShoppingBag} label="E-commerce">
+              <Sidebar.Collapse icon={HiShoppingBag} label="Funding">
                 <Sidebar.Item href="#">
-                  <h3 class="text-xl font-semibold text-gray-800">Products</h3>
+                  <h3 class="text-xl font-semibold text-gray-800">My Proposals</h3>
+                </Sidebar.Item>
+                <Sidebar.Item href="#">
+                  <h3 class="text-xl font-semibold text-gray-800">Request For Funding</h3>
                 </Sidebar.Item>
               </Sidebar.Collapse>
-              <Sidebar.Collapse icon={HiShoppingBag} label="E-commerce">
+              <Sidebar.Collapse icon={HiShoppingBag} label="Milestones">
                 <Sidebar.Item href="#">
-                  <h3 class="text-xl font-semibold text-gray-800">Products</h3>
+                  <h3 class="text-xl font-semibold text-gray-800">Completed Milestones</h3>
+                </Sidebar.Item>
+                <Sidebar.Item href="#">
+                  <h3 class="text-xl font-semibold text-gray-800">Add Milestones</h3>
                 </Sidebar.Item>
               </Sidebar.Collapse>
-              <Sidebar.Item href="#" icon={HiUser}>
-                <h3 class="text-xl font-semibold text-gray-800">Users</h3>
-              </Sidebar.Item>
-              <Sidebar.Item href="#" icon={HiShoppingBag}>
-                <h3 class="text-xl font-semibold text-gray-800">Products</h3>
-              </Sidebar.Item>
-              <Sidebar.Item href="#" icon={HiArrowSmRight}>
-                <h3 class="text-xl font-semibold text-gray-800">Sign In</h3>
-              </Sidebar.Item>
-              <Sidebar.Item href="#" icon={HiTable}>
-                <h3 class="text-xl font-semibold text-gray-800">Sign Up</h3>
-              </Sidebar.Item>
+              <Sidebar.Collapse icon={HiShoppingBag} label="Repayment">
+                <Sidebar.Item href="#">
+                  <h3 class="text-xl font-semibold text-gray-800">Repay Milestone</h3>
+                </Sidebar.Item>
+                <Sidebar.Item href="#">
+                  <h3 class="text-xl font-semibold text-gray-800">Repayments History</h3>
+                </Sidebar.Item>
+              </Sidebar.Collapse>
+              
+              
+             
             </Sidebar.ItemGroup>
           </Sidebar.Items>
         </Sidebar>
