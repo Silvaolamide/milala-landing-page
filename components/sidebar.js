2789ff5c212f8cb0e51d01d9e9db0ada296e342c
strucker-eth <aberamacarleton@gmail.com>
2023-05-21 17:08:15 +0300

    sidebar positioning

--- components/sidebar.js
+++ components/sidebar.js
@@ -61,7 +61,7 @@ const Dboard = () => {
 
   return (
     <div>
-      <div className="sidebar left-0 font-xl fixed -ml-4 w-200px rounded-xl  -pt-20">
+      <div className="sidebar left-0 font-xl fixed -ml-4 w-200px rounded-xl mt-20 pt-2">
         <Sidebar aria-label="">
           <Sidebar.Items>
             <Sidebar.ItemGroup>
