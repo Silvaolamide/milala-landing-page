f84f07d02caff82fd68afab1600fd625fbd21db6
Lennox Matwere <aberamacarleton@gmail.com>
2023-06-14 05:52:06 -0700

    Loli Milestones For Demo

--- pages/RequestFunding.js
+++ pages/RequestFunding.js
@@ -18,8 +18,8 @@ const MyProjects = () => {
           <span class="mt-1 text-gray-500 text-sm">12 Jun 2023</span>
         </div>
         <div class="md:flex-grow">
-          <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">Bitters hashtag waistcoat fashion axe chia unicorn</h2>
-          <p class="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
+          <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">Furniture & Leather(USD300)</h2>
+          <p class="leading-relaxed">This amount is needed to help procure some of furniture’s needed in your office and to purchase more leather needed in producing more bags.</p>
           <a class="text-indigo-500 inline-flex items-center mt-4">Request Funding
             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path d="M5 12h14"></path>
@@ -34,8 +34,8 @@ const MyProjects = () => {
           <span class="mt-1 text-gray-500 text-sm">13 Dec 2023</span>
         </div>
         <div class="md:flex-grow">
-          <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">Meditation bushwick direct trade taxidermy shaman</h2>
-          <p class="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
+          <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">Hiring/Human Resources (USD 420.81)</h2>
+          <p class="leading-relaxed">This is an estimated amount we would use to support the payment of workers</p>
           <a class="text-indigo-500 inline-flex items-center mt-4">Request Funding
             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path d="M5 12h14"></path>
@@ -50,8 +50,8 @@ const MyProjects = () => {
           <span class="text-sm text-gray-500">12 Jan 2024</span>
         </div>
         <div class="md:flex-grow">
-          <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">Woke master cleanse drinking vinegar salvia</h2>
-          <p class="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
+          <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">20 Sewing Machines(USD1000)</h2>
+          <p class="leading-relaxed">Currently, we need to expand production and we would like to buy more sewing machines</p>
           <a class="text-indigo-500 inline-flex items-center mt-4">Request Funding
             <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
               <path d="M5 12h14"></path>
