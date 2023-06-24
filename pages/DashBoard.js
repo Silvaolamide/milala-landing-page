6ab3e1565c8c303132740875021bcd1021266e57
Lennox Matwere <aberamacarleton@gmail.com>
2023-06-24 13:03:03 +0300

    Sidebar Items array object + Labels, links ,Icons

--- pages/DashBoard.js
+++ pages/DashBoard.js
@@ -1,12 +1,113 @@
 import React from 'react';
+import { useState } from 'react';
 import dynamic from 'next/dynamic';
 import Image from 'next/image';
 import Link from 'next/link';
 import { BsArrowRightShort, BsFillGearFill, BsSearch } from "react-icons/bs";
-import { useState } from 'react';
+import {
+    MdOutlineTune,
+    MdAllInclusive,
+    MdSettings,
+    MdNewLabel,
+     MdAccountBalanceWallet,
+    MdOutlinePayments,
+    MdSettingsInputComponent,
+    MdCancelScheduleSend,
+    MdPriceCheck,
+    MdStorage,
+    MdCheck,
+    MdOutlineMonetizationOn
+  } from "react-icons/md";
 
+  
 const DashBoard = () => {
     const [open, setOpen] = useState(true);
+
+    const Menus = [
+        {
+          id: 1,
+          label: 'Projects Management',
+          icon: MdOutlineTune,
+          link: '/',
+        },
+        {
+          id: 2,
+          label: 'My Projects',
+          icon: MdAllInclusive,
+          link: '/my-projects',
+        },
+        {
+          id: 3,
+          label: "Projects' Management",
+          icon: MdSettings,
+          submenus: [
+            {
+              id: 31,
+              label: 'Add Project',
+              icon: MdNewLabel,
+              link: '/AddProject',
+            },
+            {
+              id: 32,
+              label: 'Add Milestones',
+              icon: MdSettingsInputComponent,
+              link: '/AddMilestones',
+            },
+          ],
+        },
+        {
+          id: 4,
+          label: 'Funding',
+          icon: MdAccountBalanceWallet,
+          submenus: [
+            {
+              id: 41,
+              label: 'Request For Funding',
+              icon: MdCancelScheduleSend,
+              link: '/RequestFunding',
+            },
+          ],
+        },
+        {
+          id: 5,
+          label: 'Milestones',
+          icon: MdStorage,
+          submenus: [
+            {
+              id: 51,
+              label: 'Completed Milestones',
+              icon: MdCheck,
+              link: '/CompletedMilestones',
+            },
+            {
+              id: 52,
+              label: 'Update Milestones',
+              icon: MdNewLabel,
+              link: '/add-milestones',
+            },
+          ],
+        },
+        {
+          id: 6,
+          label: 'Repayment',
+          icon: MdPriceCheck,
+          submenus: [
+            {
+              id: 61,
+              label: 'Repay Milestone',
+              icon: MdOutlineMonetizationOn,
+              link: '/repay-milestone',
+            },
+            {
+              id: 62,
+              label: 'Repayments History',
+              icon: MdOutlinePayments,
+              link: '/repayment-history',
+            },
+          ],
+        },
+      ];
+    
 
   return (
     <>
@@ -34,6 +135,10 @@ const DashBoard = () => {
   <input type="search" placeholder="Search" className={`text-sm bg-transparent w-full text-white focus:outline-none focus:ring-0 placeholder:text-white ${!open && "hidden"}`} />
 </div>
 
+
+<ul>
+
+</ul>
 
 </div>
 
