b78697ed50949c9c95b414d9ab67433b41d1f775
Lennox Matwere <aberamacarleton@gmail.com>
2023-06-24 11:31:05 +0300

    Collapsing Sidebar on Icon Cick

--- pages/DashBoard.js
+++ pages/DashBoard.js
@@ -3,13 +3,17 @@ import dynamic from 'next/dynamic';
 import Image from 'next/image';
 import Link from 'next/link';
 import { BsArrowLeftShort } from "react-icons/bs";
+import { useState } from 'react';
 
 const DashBoard = () => {
+    const [open, setOpen] = useState(true);
+
   return (
 <div class="flex">
-  <div class="h-screen bg-teal-900 p-5 pt-8 w-64 relative">
+  <div class={`h-screen bg-teal-900 p-5 pt-8 ${open ? "w-64" : "w-20"} relative`}>
+
   <BsArrowLeftShort class="bg-white text-teal-950 text-3xl rounded-full absolute -right-3
-  top-8 border border-teal-950 cursor-pointer"
+  top-8 border border-teal-950 cursor-pointer" onClick={() => setOpen (!open)}
    /> 
   DashBoard
 
