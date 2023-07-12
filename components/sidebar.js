c770899c6316a42fdbbe5431d4b9714298ce1636
Lennox Matwere <aberamacarleton@gmail.com>
2023-07-12 13:52:22 +0300

    sidebar component for common dashboard

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,114 +1,85 @@
-import React from 'react';
-import { Sidebar } from 'flowbite-react';
-import { useRouter } from 'next/router';
-import { Flowbite } from 'flowbite-react';
-import {
-  MdOutlineTune,
-  MdAllInclusive,
-  MdSettings,
-  MdNewLabel,
-   MdAccountBalanceWallet,
-  MdOutlinePayments,
-  MdSettingsInputComponent,
-  MdCancelScheduleSend,
-  MdPriceCheck,
-  MdStorage,
-  MdCheck,
-  MdOutlineMonetizationOn
-} from "react-icons/md";
-
-
-
+import React, { useState } from 'react';
 import dynamic from 'next/dynamic';
-import Image from 'next/image';
+import { BsArrowRightShort, BsFillGearFill, BsSearch, BsChevronDown } from 'react-icons/bs';
+import Menus from '../components/menuItems';
 import Link from 'next/link';
-import MyProjects from '../pages/MyProjects';
-import AddProject from '../pages/AddProject';
-import UpdateMilestones from '../pages/UpdateMilestones';
-import RequestFunding from '../pages/RequestFunding';
-import CompletedMilestones from '../pages/CompletedMilestones';
-import AddMilestones from '../pages/AddMilestones';
-import RepayMilestone from '../pages/RepayMilestone';
-import RepaymentHistory from '../pages/RepaymentHistory';
 
-const Dboard = () => {
-  const router = useRouter();
+const sideBar = () => {
+  const [open, setOpen] = useState(true);
+  const [submenuOpen, setSubmenuOpen] = useState({});
 
-  const renderPage = () => {
-    const { pathname } = router;
-
-    switch (pathname) {
-      case '/':
-        return <MyProjects />;
-      case '/AddProject':
-        return <AddProject />;
-      case '/UpdateMilestones':
-        return <UpdateMilestones />;
-      case '/RequestFunding':
-        return <RequestFunding />;
-      case '/CompletedMilestones':
-        return <CompletedMilestones />;
-      case '/AddMilestones':
-        return <AddMilestones />;
-      case '/RepayMilestone':
-        return <RepayMilestone />;
-      case '/RepaymentHistory':
-        return <RepaymentHistory />;
-      default:
-        return null;
-    }
+  const handleSubmenuToggle = (menuId) => {
+    setSubmenuOpen((prevState) => ({
+      ...prevState,
+      [menuId]: !prevState[menuId],
+    }));
   };
 
   return (
-    <div>
-      <div className="sidebar left-0 font-xl fixed -ml-4 w-200px rounded-xl mt-20 pt-2">
-        <Sidebar aria-label="">
-          <Sidebar.Items>
-            <Sidebar.ItemGroup>
-              <Sidebar.Item href="/" icon={MdOutlineTune}>
-                <h1 className="text-xl font-semibold text-black ml-3 ">Projects Management</h1>
-              </Sidebar.Item>
-              <Sidebar.Item href="/my-projects" icon={MdAllInclusive}>
-                <h3 className="text-xl font-semibold text-black ml-3">My Projects</h3>
-              </Sidebar.Item>
-              <Sidebar.Collapse icon={MdSettings} label="Projects' Management">
-                <Sidebar.Item href="/AddProject" icon={MdNewLabel}>
-                  <h3 className="text-xl font-semibold text-black ml-3">Add Project</h3>
-                </Sidebar.Item>
-                <Sidebar.Item href="/AddMilestones" icon={MdSettingsInputComponent}>
-                  <h3 className="text-xl font-semibold text-black ml-3">Add Milestones</h3>
-                </Sidebar.Item>
-              </Sidebar.Collapse>
-              <Sidebar.Collapse icon={MdAccountBalanceWallet} label="Funding">
-                <Sidebar.Item href="/RequestFunding" icon={MdCancelScheduleSend}>
-                  <h3 className="text-xl font-semibold text-black ml-3">Request For Funding</h3>
-                </Sidebar.Item>
-              </Sidebar.Collapse>
-              <Sidebar.Collapse icon={MdStorage} label="Milestones">
-                <Sidebar.Item href="/CompletedMilestones" icon={MdCheck}>
-                  <h3 className="text-xl font-semibold text-black ml-3">Completed Milestones</h3>
-                </Sidebar.Item>
-                <Sidebar.Item href="/add-milestones" icon={MdNewLabel}>
-                  <h3 className="text-xl font-semibold text-black ml-3 ">Update Milestones</h3>
-                </Sidebar.Item>
-              </Sidebar.Collapse>
-              <Sidebar.Collapse icon={MdPriceCheck} label="Repayment">
-                <Sidebar.Item href="/repay-milestone" icon={MdOutlineMonetizationOn}>
-                  <h3 className="text-xl font-semibold text-gray-900 ml-3">Repay Milestone</h3>
-                </Sidebar.Item>
-                <Sidebar.Item href="/repayment-history" icon={MdOutlinePayments}>
-                  <h3 className="text-xl font-semibold text-black ml-3">Repayments History</h3>
-                </Sidebar.Item>
-              </Sidebar.Collapse>
-            </Sidebar.ItemGroup>
-          </Sidebar.Items>
-        </Sidebar>
+    <>
+      <div className="flex">
+        <div className={`h-screen bg-teal-900 p-5 pt-8 ${open ? 'w-64' : 'w-20'} duration-300 relative`}>
+          <BsArrowRightShort
+            className={`bg-white text-teal-950 text-3xl rounded-full absolute ml-3 -right-3 top-8 border border-teal-950 cursor-pointer duration-300 ${
+              !open && 'rotate-180'
+            }`}
+            onClick={() => setOpen(!open)}
+          />
+          <div className="inline-flex -pt-3">
+            <BsFillGearFill
+              className={`bg-white mr-2 text-3xl text-teal-950 border rounded-full cursor-pointer block float-left duration-500 ${
+                open && 'rotate-[360deg]'
+              }`}
+            />
+            <h1 className={`text-white font-bold text-2xl duration-300 -mt-1 ml-2 ${!open && 'scale-0'}`}>Milala</h1>
+          </div>
+
+          <div className={`flex items-center rounded-md bg-[#FFFFF7] mt-6 ${!open ? 'px-2.5' : 'px-4'} py-2 bg-opacity-30 backdrop-filter backdrop-blur-lg`}>
+            <BsSearch className={`text-white text-xl block float-left cursor-pointer ${open && 'mr-2'}`} />
+            <input
+              type="search"
+              placeholder="Search"
+              className={`text-sm bg-transparent w-full text-white focus:outline-none focus:ring-0 placeholder:text-white ${!open && 'hidden'}`}
+            />
+          </div>
+
+          <ul>
+            {Menus.map((menu) => (
+              <li
+                key={menu.id}
+                className={`text-white text-m flex flex-col gap-y-2 cursor-pointer p-2 ${
+                  submenuOpen[menu.id] ? '' : 'hover:bg-slate-300 hover:text-teal-900 hover:rounded-lg'
+                } mt-2`}
+              >
+                <div className="flex items-center gap-x-2">
+                  <span className="text-3xl">{menu.icon}</span>
+                  <span className={`text-base ${!open && 'hidden'}`}>{menu.label}</span>
+                  {menu.submenus && open && (
+                    <BsChevronDown
+                      className={`${submenuOpen[menu.id] ? 'rotate-180' : ''} ${open && 'display'}`}
+                      onClick={() => handleSubmenuToggle(menu.id)}
+                    />
+                  )}
+                </div>
+
+                {menu.submenus && submenuOpen[menu.id] && open && (
+                  <ul className="text-white text-sm ml-4">
+                    {menu.submenus.map((submenu) => (
+                      <li key={submenu.id} className="">
+                        <div className="flex p-2 items-center">
+                          <Link href={submenu.link}>{submenu.label}</Link>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div className="content ml-220px">
-        {renderPage()}
-      </div>
-    </div>
+    </>
   );
 };
 
-export default dynamic(() => Promise.resolve(Dboard), { ssr: false });
+export default sideBar;
