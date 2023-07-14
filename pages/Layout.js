b830673fa95ae8e8dd992b1c56f7d4428a6ad015
Lennox Matwere <aberamacarleton@gmail.com>
2023-07-14 07:35:59 +0300

    navbar Links' tags fix from <a> to <Link>

--- pages/Layout.js
+++ pages/Layout.js
@@ -4,6 +4,7 @@ import dynamic from 'next/dynamic';
 import Image from 'next/image';
 import sideBar from '../components/sidebar';
 import DashBoard from './DashBoard';
+import Link from 'next/link'
 
 const commonLayout = ({ children }) => {
   return (
@@ -13,10 +14,10 @@ const commonLayout = ({ children }) => {
      <section id="Navbar" class="rounded-xl   w-full">
     <nav class="rounded-xl bg-white fixed w- -mt-3 top-4 right-3 left-3 ">
       <div class="max-w-screen-xl flex flex-wrap items-center  -pb-3 justify-between mx-auto p-2">
-      <a href="/" class="flex items-center">
+      <Link href="/" class="flex items-center">
           <Image src="./img/milala-logo.svg" class="h-12 w-20 mt-3 mr-3" alt="Milala Logo" height={20} width={20}></Image>
           <span class="self-center text-[#009A9A] -ml-7 font-inter text-2xl font-semibold whitespace-nowrap ">Milala</span>
-      </a>
+      </Link>
       <div class="flex md:order-2">
           <button type="button" class="text-gray  focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 "> <ConnectButton >  </ConnectButton> </button>
           <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-transparent focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
@@ -27,16 +28,16 @@ const commonLayout = ({ children }) => {
       <div class="items-center justify-between hidden w-full  md:flex md:w-auto md:order-1" id="navbar-sticky">
         <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-transparent ">
           <li>
-            <a href="/" class="block py-2 pl-3 pr-4 text-gray bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
+            <Link href="/" class="block py-2 pl-3 pr-4 text-gray bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</Link>
           </li>
           <li>
-            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-transparent md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-gray dark:hover:bg-gray-700 dark:hover:text-gray md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+            <Link href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-transparent md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-gray dark:hover:bg-gray-700 dark:hover:text-gray md:dark:hover:bg-transparent dark:border-gray-700">About</Link>
           </li>
           <li>
-            <a href="/projects" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-transparent md:hover:bg-transparent md:p-0 md:dark:hover:text-blue-500 dark:text-gray dark:hover:bg-gray-700 dark:hover:text-gray md:dark:hover:bg-transparent dark:border-gray-700">Projects</a>
+            <Link href="/projects" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-transparent md:hover:bg-transparent md:p-0 md:dark:hover:text-blue-500 dark:text-gray dark:hover:bg-gray-700 dark:hover:text-gray md:dark:hover:bg-transparent dark:border-gray-700">Projects</Link>
           </li>
           <li>
-            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-transparent md:hover:bg-transparent md:p-0 ">Contact</a>
+            <Link href="#" class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-transparent md:hover:bg-transparent md:p-0 ">Contact</Link>
           </li>
         </ul>
       </div>
