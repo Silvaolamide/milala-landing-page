036bf6ca20da7777bfa88f37a9a01c99cac18735
Lennox Matwere <aberamacarleton@gmail.com>
2023-07-14 16:27:04 +0300

    Navbar  border radius to 20px, top repositioning

--- pages/index.js
+++ pages/index.js
@@ -18,21 +18,21 @@ export default function Home() {
     </head>
         
     <section id="Navbar" class="rounded-xl fixed top-0 w-full">
-    <nav class="rounded-xl bg-white fixed w-10/12 -mt-3 top-4 lg:left-28  right-3 left-3">
+    <nav class="rounded-[20px] bg-white fixed w-10/12 -mt-4 top-4 lg:left-28  right-3 left-3">
       <div class="max-w-screen-xl flex flex-wrap items-center  -pb-3 justify-between mx-auto p-2">
       <Link href="/" class="flex sm:items-center items-center">
           <Image src="./img/milala-logo.svg" class="h-12 w-20 mt-3 mr-3" alt="Milala Logo" height={20} width={20}></Image>
           <span class="self-center text-[#009A9A] -ml-7 font-inter text-2xl  font-semibold whitespace-nowrap ">Milala</span>
       </Link>
       <div class="flex md:order-2">
-          <div class="text-gray text-sm px-4 py-2 text-center mr-3 md:mr-0 "> <ConnectButton >  </ConnectButton> </div>
+          <div class="text-gray text-sm px-4 py-2 text-center mr-3 md:mr-0 "> <ConnectButton class="rounded-full">  </ConnectButton> </div>
           <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-transparent focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
             <span class="sr-only">Open main menu</span>
             <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
           </button>
       </div>
       <div class="items-center justify-between hidden w-full  md:flex md:w-auto md:order-1" id="navbar-sticky">
-        <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-transparent ">
+        <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100  md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-transparent ">
           <li>
             <Link href="/" class="block py-2 pl-3 pr-4 text-gray bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</Link>
           </li>
