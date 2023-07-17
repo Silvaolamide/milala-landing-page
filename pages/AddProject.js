4dff2adf359e722c53465d50e1cf2d879893c04a
Lennox Matwere <aberamacarleton@gmail.com>
2023-07-17 12:06:57 +0300

    Spacing form fields and removing extra component

--- pages/AddProject.js
+++ pages/AddProject.js
@@ -142,14 +142,6 @@ const MyProjects = () => {
   />
 </div>
 
-<div>
-    <Button gradientMonochrome="teal">
-      Teal
-    </Button>
-  </div>
-
-
-
   <div class="my-5 flex mr-10 ml-10 items-center justify-center">
     <Link href="/DashBoard">
   <button class=" mt-2 rounded-lg border-2 border-[#009A9A] bg-[#009A9A] px-6 py-2 font-medium text-white justify-center items-center transition hover:translate-y-1">
